593edeb11f6b3d76576e18b36a12efe725399a3f
Masaki Yatsu <yatsu@yatsu.info>
2010-04-10 20:54:48 +0900

    [sample] Moved labels to the center.
    [readme] Modified the image/screencast url.

--- TreemapKit/TreemapViewCell.m
+++ TreemapKit/TreemapViewCell.m
@@ -16,23 +16,25 @@
 		self.layer.borderWidth = 1.0;
 		self.layer.borderColor = [[UIColor whiteColor] CGColor];
 
-		self.valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, frame.size.height - 30, frame.size.width - 4, 14)];
-		valueLabel.font = [UIFont boldSystemFontOfSize:16];
-		valueLabel.textAlignment = UITextAlignmentRight;
-		valueLabel.textColor = [UIColor whiteColor];
-		valueLabel.backgroundColor = [UIColor clearColor];
-		valueLabel.lineBreakMode = UILineBreakModeCharacterWrap;
-		valueLabel.adjustsFontSizeToFitWidth = frame.size.width - 4;
-		[self addSubview:valueLabel];
-
-		self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, frame.size.height - 17, frame.size.width - 4, 14)];
-		textLabel.font = [UIFont boldSystemFontOfSize:16];
-		textLabel.textAlignment = UITextAlignmentRight;
+		self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width - 4, 20)];
+		textLabel.font = [UIFont boldSystemFontOfSize:20];
+		textLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+		textLabel.textAlignment = UITextAlignmentCenter;
 		textLabel.textColor = [UIColor whiteColor];
 		textLabel.backgroundColor = [UIColor clearColor];
 		textLabel.lineBreakMode = UILineBreakModeCharacterWrap;
-		textLabel.adjustsFontSizeToFitWidth = frame.size.width - 4;
+		textLabel.adjustsFontSizeToFitWidth = YES;
 		[self addSubview:textLabel];
+
+		self.valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width - 4, 20)];
+		valueLabel.font = [UIFont boldSystemFontOfSize:20];
+		valueLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+		valueLabel.textAlignment = UITextAlignmentCenter;
+		valueLabel.textColor = [UIColor whiteColor];
+		valueLabel.backgroundColor = [UIColor clearColor];
+		valueLabel.lineBreakMode = UILineBreakModeCharacterWrap;
+		valueLabel.adjustsFontSizeToFitWidth = YES;
+		[self addSubview:valueLabel];
 	}
 	return self;
 }
@@ -40,8 +42,8 @@
 - (void)layoutSubviews {
 	[super layoutSubviews];
 
-	valueLabel.frame = CGRectMake(0, self.frame.size.height - 30, self.frame.size.width - 4, 14);
-	textLabel.frame = CGRectMake(0, self.frame.size.height - 17, self.frame.size.width - 4, 14);
+	textLabel.frame = CGRectMake(0, self.frame.size.height / 2 - 10, self.frame.size.width, 20);
+	valueLabel.frame = CGRectMake(0, self.frame.size.height / 2 + 10, self.frame.size.width, 20);
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
